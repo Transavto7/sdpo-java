d988c9ccd0d884a8aa556ba77f0a3799fb0f9391
K.Budyukin <budu4ka@gmail.com>
2024-02-05 23:39:44 +0300

    feat: Изменена верисия на 2.5.6. Пересобран бандл

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -24,7 +24,7 @@ export default {
     </transition>
     <div class="footer">
       {{ point || 'Незвестный пункт выпуска' }}
-      <span>СДПО 2.5.5</span>
+      <span>СДПО 2.5.6</span>
     </div>
 </template>
 
