d09a18ae045fa85cf2c2df60157936e9eb588a5e
K.Budyukin <budu4ka@gmail.com>
2024-02-15 22:47:32 +0300

    feat: Изменен интерфейс с учетом последних правок.

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -18,23 +18,6 @@ export default {
     serialNumber() {
       return this.$store.state.verification.serialNumberTerminal
     },
-    needVerification() {
-      if (!this.dateVerification) return false;
-
-      let now = new Date();
-      let need = new Date(this.dateVerification | '')
-      return (need - now) < 0
-    },
-    hasMonthIntoVerification() {
-      if (!this.dateVerification) return false;
-
-      let now = new Date();
-      let need = new Date(this.dateVerification | '')
-      return (need - now) < (1000 * 60 * 60 * 24 * 30) && (need > now)
-    },
-    title() {
-      return !this.hasMonthIntoVerification ? 'Обратитесь к администратору до поверки осталось меньше месяца' : '';
-    }
   },
 }
 </script>
@@ -48,13 +31,11 @@ export default {
   <div class="footer">
     <div class="footer__point-version">
       {{ point || 'Незвестный пункт выпуска' }}
-      <span>СДПО 2.5.7</span>
+      <span>Версия: 2.5.7</span>
     </div>
   </div>
   <div class="footer__serial-number_date-notification">
     <div>
-      <span :hidden="{needVerification}" class="danger">НЕОБХОДИМО ПРОЙТИ ПОВЕРКУ</span>
-      <span :class="{danger : hasMonthIntoVerification}" :hidden="{hasMonthIntoVerification}" class="date-notification">До поверки устройства осталось меньше месяца</span>
       <span class="date-notification">{{dateVerification || ''}}</span>
     </div>
     <span class="serial-number">{{(serialNumber ? 'S/N ' + serialNumber : '') }}</span>
