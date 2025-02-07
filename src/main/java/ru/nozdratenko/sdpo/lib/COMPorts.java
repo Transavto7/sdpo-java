a1569e2b340e2f37fae2d4780ae483aec2103d07
Vladimir Sankin <baar1809@gmail.com>
2025-02-08 00:32:36 +0300

    wip: разделение сборки для разработки

--- src/main/java/ru/nozdratenko/sdpo/lib/COMPorts.java
+++ src/main/java/ru/nozdratenko/sdpo/lib/COMPorts.java
@@ -4,15 +4,15 @@ import ru.nozdratenko.sdpo.file.FileBase;
 
 public class COMPorts {
 
-//    static {
-//        String url = FileBase.exportLibrary("com-win-cpp.dll");
-//        System.load(url.replace("\\", "/"));
-//    }
-//
-//    public static native String getComPort(String vid);
-
-    public static String getComPort(String vid)
-    {
-        return "testing";
+    static {
+        String url = FileBase.exportLibrary("com-win-cpp.dll");
+        System.load(url.replace("\\", "/"));
     }
+
+    public static native String getComPort(String vid);
+
+//    public static String getComPort(String vid)
+//    {
+//        return "testing";
+//    }
 }
