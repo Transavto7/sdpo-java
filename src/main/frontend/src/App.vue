f696d1f23e00c790dfe8de13c37b8ca0ecf1f805
Vladimir Sankin <baar1809@gmail.com>
2025-04-14 21:52:48 +0300

    fix: Исправление версии

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -39,7 +39,7 @@ export default {
   <div class="footer">
     <div class="footer__point-version">
       {{ point || 'Незвестный пункт выпуска' }}
-      <span>Версия: 2.10.0</span>
+      <span>Версия: 2.10.1</span>
     </div>
   </div>
   <div class="footer__serial-number_date-notification">
