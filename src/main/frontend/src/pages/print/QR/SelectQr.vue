2c8ee17817f41995c5cb0c81c14b8e9315fdc552
Vladimir Sankin <baar1809@gmail.com>
2025-08-02 14:29:37 +0300

    feat: Добавлен QR с оплатой

--- src/main/frontend/src/pages/print/QR/SelectQr.vue
+++ src/main/frontend/src/pages/print/QR/SelectQr.vue
@@ -1,6 +1,11 @@
 <script>
 
 export default {
+  data(){
+    return {
+      showPayments: false,
+    }
+  },
   methods: {
     selectBus() {
       this.$emit('select', 'carId');
@@ -8,23 +13,44 @@ export default {
     selectDriver() {
       this.$emit('select', 'driverId');
     },
+    selectBank() {
+      this.showPayments = true;
+    },
   }
 }
 </script>
 
 <template>
-  <div class="print-selection">
-    <div class="tab animate__animated animate__fadeInUp d-1" @click="selectBus()">
-      <img width="300" src="@/assets/images/print/busQR.svg">
-      <label style="margin-top: 15px;">QR инспекции автомобиля</label>
+  <div>
+    <div v-if="!showPayments" class="print-selection">
+      <div class="tab animate__animated animate__fadeInUp d-1" @click="selectBus()">
+        <img width="250" src="@/assets/images/print/busQR.svg">
+        <label style="margin-top: 15px;">QR инспекции автомобиля</label>
+      </div>
+      <div class="tab animate__animated animate__fadeInUp d-1" @click="selectDriver()">
+        <img width="250" src="@/assets/images/print/driverQR.svg">
+        <label style="margin-top: 15px;">QR осмотра водителя</label>
+      </div>
+      <div class="tab animate__animated animate__fadeInUp d-1" @click="selectBank()">
+        <img width="250" src="@/assets/images/print/bankQR.svg">
+        <label style="margin-top: 15px;">Оплата услуг</label>
+      </div>
     </div>
-    <div class="tab">
-      <img width="300" src="@/assets/images/print/driverQR.svg" @click="selectDriver()">
-      <label style="margin-top: 15px;">QR осмотра водителя</label>
+    <div v-if="showPayments" class="payment-show">
+      <img height="500" alt="QR не найден обратитесь к администратору" src="@/assets/images/print/payment_qr.webp">
     </div>
   </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.payment-show {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-weight: 500;
+  gap: 1.3rem;
+}
 </style>
